f38ca13267b65c684fe528b9e0f1c75beb7913d9
natew <nateweil1993@gmail.com>
2023-01-24 14:20:37 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def home():
+    return render_template("home.html")
+
+@app.route("/api/v1/<station>/<date>")
+def about(station, date):
+
+    # data = pandas.read_csv("")
+    # temperature = data.station(date)
+    temperature = 23
+    # return render_template("about.html")
+    return {"Station: ": station,
+            "Date: ": date,
+            "Temperature: ": temperature}
+
+if __name__ == "__main__":
+    app.run(debug=True)
